02e67f070a7fe4dcf71cdc513d86d8af5ccd0f28
Shukurah Bakare <shukurahbakare@outlook.com>
2021-11-25 22:31:29 +0100

    Push data into an array and iterate over

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormControlName, FormGroup } from '@angular/forms';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -7,17 +8,46 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  closeResult = '';
+  diaryForm!:FormGroup
+  title:any;
+  text:any;
+  modalReference:any;
+  editData:any;
+  diaries = [
+    {title:'My Boyfriend',text:'He loves me so much but i am not sure i feel the same,i kinda like the idea of him but no him'},
+    {title:'My Product Manager',text:'He gives me a lot of work which is sometomes too much but help my growth'},
+    {title:'Bad Day',text:'I had my fair share of lagos madness today'},
+    {title:'My Boyfriend',text:'He loves me so much but i am not sure i feel the same,i kinda like the idea of him but no him'},
+    {title:'My Boyfriend',text:'He loves me so much but i am not sure i feel the same,i kinda like the idea of him but no him'}
+  ]
 
   constructor(private modalService: NgbModal) { }
+  
   open(content:any) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      
-    }, (reason) => {
-      
+    this.modalReference = this.modalService.open(content)
+    this.modalReference.result.then(() => {  
+    }, () => { 
     });
   }
+  
 
   ngOnInit(): void {
+    this.diaryForm = new FormGroup({
+      title:new FormControl(''),
+      text:new FormControl('')
+    })
   }
+
+
+  onSave() {
+    this.diaries.push(this.diaryForm.value)
+    this.modalReference.close();
+  }
+
+  onEdit(data:any){
+    this.editData = data;
+    console.log('Here is the data');
+    
+  }
+ 
 }
